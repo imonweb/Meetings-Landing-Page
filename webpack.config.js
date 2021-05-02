f25b55465171d2106d1e03fccb39a274b7502960
imonweb <contact@imonweb.co.uk>
2021-05-02 23:17:28 +0100

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'myBundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader","css-loader"]
+      },
+      {
+        test: /\.js$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          }
+        }
+      },
+      {
+        test: /\.(jpg|jpeg|svg|gif)$/i,
+        type: 'asset/resource' 
+      }
+    ]
+  },
+  devServer: {
+    before: function(src, server){
+      server._watch('./src/*.html')
+    },
+    contentBase: path.join(__dirname,'src'),
+    hot: true,
+    port: 3000
+  }
+}
